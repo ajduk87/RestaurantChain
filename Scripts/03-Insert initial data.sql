1aac10ace14e5097685f0db157834db583719da4
nikolaajdukovic <ajduk87@gmail.com>
2022-09-25 09:11:45 +0200

    added database and configuration file

---
+++ Scripts/03-Insert initial data.sql
@@ -0,0 +1,121 @@
+INSERT INTO restaurantchain.menuitems(
+    id,name, description, price, ismeal)
+	VALUES (1, 'Veal soup', 'Soup made from veal joint', 3.54, false);
+	
+INSERT INTO restaurantchain.menuitems(
+    id,name, description, price, ismeal)
+	VALUES (2, 'Chicken soup', 'Soup made from chicken back', 2.76, false);
+	
+INSERT INTO restaurantchain.menuitems(
+    id,name, description, price, ismeal)
+	VALUES (3, 'Crispy chicken', 'Chicken steak with almonds, walnuts and sour apple sauce', 7.87, false);
+	
+INSERT INTO restaurantchain.menuitems(
+    id,name, description, price, ismeal)
+	VALUES (4, 'Stuffed chicken', 'Chicken stuffed with roasted peppers and bacon', 7.21, false);
+	
+INSERT INTO restaurantchain.menuitems(
+    id,name, description, price, ismeal)
+	VALUES (5, 'Grilled turkey fillet', 'Grilled turkey fillet with mesh potato', 7.93, false);
+	
+INSERT INTO restaurantchain.menuitems(
+    id,name, description, price, ismeal)
+	VALUES (6, 'Mixed meat', 'Mixed crilled chichen fillet, sausage and hamburger with fries and onion', 8.79, false);
+	
+INSERT INTO restaurantchain.menuitems(
+    id,name, description, price, ismeal)
+	VALUES (7, 'Tomato salad', 'Salad of chopped tomatoes on rings seasoned with olive oil', 2.59, false);
+	
+INSERT INTO restaurantchain.menuitems(
+    id,name, description, price, ismeal)
+	VALUES (8, 'Cabbage salad', 'Shredded cabbage salad seasoned with vinegar', 2.42, false);
+	
+INSERT INTO restaurantchain.menuitems(
+    id,name, description, price, ismeal)
+	VALUES (9, 'White bread', 'Bread made from wheat', 1.34, false);
+	
+INSERT INTO restaurantchain.menuitems(
+    id,name, description, price, ismeal)
+	VALUES (10, 'Brown bread', 'Bread made from rye', 1.85, false);
+	
+INSERT INTO restaurantchain.menuitems(
+    id,name, description, price, ismeal)
+	VALUES (11, 'Spring meal', 'Chicken soup and Crispy chicken with Tomato salad and White bread', null, true);
+	
+INSERT INTO restaurantchain.menuitems(
+    id,name, description, price, ismeal)
+	VALUES (12, 'Summer meal', 'Chicken soup and Grilled turkey fillet with Tomato salad and White bread', null, true);
+	
+INSERT INTO restaurantchain.menuitems(
+    id,name, description, price, ismeal)
+	VALUES (13, 'Autumn meal', 'Veal soup and Stuffed chicken with Cabbage salad and Brown bread', null, true);
+	
+INSERT INTO restaurantchain.menuitems(
+    id,name, description, price, ismeal)
+	VALUES (14, 'Winter meal', 'Veal soup and Mixed meat with Cabbage salad and Brown bread', null, true);
+	
+	
+	
+INSERT INTO restaurantchain.mealsdishes(
+	mealid, dishid)
+	VALUES (11, 2);
+	
+INSERT INTO restaurantchain.mealsdishes(
+	mealid, dishid)
+	VALUES (11, 3);
+	
+INSERT INTO restaurantchain.mealsdishes(
+	mealid, dishid)
+	VALUES (11, 7);
+	
+INSERT INTO restaurantchain.mealsdishes(
+	mealid, dishid)
+	VALUES (11, 9);
+	
+INSERT INTO restaurantchain.mealsdishes(
+	mealid, dishid)
+	VALUES (12, 2);
+	
+INSERT INTO restaurantchain.mealsdishes(
+	mealid, dishid)
+	VALUES (12, 5);
+	
+INSERT INTO restaurantchain.mealsdishes(
+	mealid, dishid)
+	VALUES (12, 7);
+	
+INSERT INTO restaurantchain.mealsdishes(
+	mealid, dishid)
+	VALUES (12, 9);
+	
+INSERT INTO restaurantchain.mealsdishes(
+	mealid, dishid)
+	VALUES (13, 1);
+	
+INSERT INTO restaurantchain.mealsdishes(
+	mealid, dishid)
+	VALUES (13, 4);
+	
+INSERT INTO restaurantchain.mealsdishes(
+	mealid, dishid)
+	VALUES (13, 8);
+	
+INSERT INTO restaurantchain.mealsdishes(
+	mealid, dishid)
+	VALUES (13, 10);
+	
+INSERT INTO restaurantchain.mealsdishes(
+	mealid, dishid)
+	VALUES (14, 1);
+	
+INSERT INTO restaurantchain.mealsdishes(
+	mealid, dishid)
+	VALUES (14, 6);
+	
+INSERT INTO restaurantchain.mealsdishes(
+	mealid, dishid)
+	VALUES (14, 8);
+	
+INSERT INTO restaurantchain.mealsdishes(
+	mealid, dishid)
+	VALUES (14, 10);
